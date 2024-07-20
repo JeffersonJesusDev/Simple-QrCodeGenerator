3676fa8b4b011ac79164c46d7e5ff34fe4a39655
JeffersonJesusDev <jefferson3680@hotmail.com>
2024-07-20 15:03:13 -0300

    Featch: Start Project

---
+++ src/main/java/br/com/jeffdev/qrcodegenarator/demo/QrcodegeneratorApplication.java
@@ -0,0 +1,13 @@
+package br.com.jeffdev.qrcodegenarator.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrcodegeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrcodegeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jeffdev/qrcodegenarator/demo/QrcodegeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jeffdev.qrcodegenarator.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrcodegeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
