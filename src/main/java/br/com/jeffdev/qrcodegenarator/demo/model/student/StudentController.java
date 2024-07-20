eea4e86d1f4a1b59b524265041ed9226616e6276
JeffersonJesusDev <jefferson3680@hotmail.com>
2024-07-20 17:24:55 -0300

    Modificando meu método para retornar os QrCodes com as informações de todos os estudantes

--- src/main/java/br/com/jeffdev/qrcodegenarator/demo/model/student/StudentController.java
+++ src/main/java/br/com/jeffdev/qrcodegenarator/demo/model/student/StudentController.java
@@ -1,9 +1,12 @@
 package br.com.jeffdev.qrcodegenarator.demo.model.student;
 
+import br.com.jeffdev.qrcodegenarator.demo.utils.QrCodeGenerator;
+import com.google.zxing.WriterException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -13,7 +16,13 @@ public class StudentController {
     private final StudentService studentService;
 
     @GetMapping
-    public ResponseEntity<List<Student>> getAllStudents() {
+    public ResponseEntity<List<Student>> getAllStudents() throws IOException, WriterException {
+        List<Student> students = studentService.getAllStudents();
+        if (students.size() > 0) {
+            for (Student student : students) {
+                QrCodeGenerator.generateQRCode(student);
+            }
+        }
         return ResponseEntity.ok(studentService.getAllStudents());
     }
 
